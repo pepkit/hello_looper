581573fb4ad7aadfe67ee5142cdd15e4a0c3b555
Donald Campbell <125581724+donaldcampbelljr@users.noreply.github.com>
2023-08-30 11:36:48 -0400

    move pipestat_example out of subfolder

--- pipeline_pipestat/count_lines.py
+++ pipeline_pipestat/count_lines.py
@@ -7,22 +7,20 @@ text_file = sys.argv[1] # this is the sample we wish to process by reading the n
 sample_name = sys.argv[2]
 schema_path = sys.argv[3]
 results_file = sys.argv[4]
-pipeline_type = sys.argv[5]
 
 # Create pipestat manager and then report values
 psm = pipestat.PipestatManager(
     schema_path=schema_path,
     results_file_path=results_file,
     sample_name=sample_name,
-    pipeline_type=pipeline_type,
+    pipeline_type="sample",
 )
 
 # Read text file and count lines
 with open(text_file, "r") as f:
-    number_of_lines = len(f.readlines())
+    result = {"number_of_lines": len(f.readlines())}
 
 # The results are defined in the pipestat output schema.
-result = {"number_of_lines": number_of_lines}
 psm.report(sample_name=sample_name, values=result)
 
 # end of pipeline
